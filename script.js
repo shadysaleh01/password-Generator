632554c7cfdf7f9ba5067787692db23d8451ac1a
Shady Coder <shadysaleh01@gmail.com>
2020-07-21 23:54:27 -0700

    finished javascript and all project

--- script.js
+++ script.js
@@ -1,120 +1,40 @@
-var special = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "'",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  " < ",
-  "=",
-  " > ",
-  " ? ",
-  "@",
-  "[",
-  "\\",
-  "]",
-  " ^ ",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
-];
+// spacial characters
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+// numeric characters
 var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var lowercase = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var uppercase = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+// alphaberical lowercase characters
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// alphaberical uppercase characters
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// var conSpecial;
-// var conNumber;
-// var conLowercase;
-// var conUppercase;
+// user confirm varibles
+var conSpecial;
+var conNumber;
+var conLowercase;
+var conUppercase;
+var userChoices;
+var user;
 
-//generate random password
+//function generate random password
 function generator() {
-  var user = prompt(
+  // start user input prompt
+  user = prompt(
     "How many characters whould you like your password to contain? choose between 8 and 128"
   );
 
   if (user < 8 || user > 128) {
     user = prompt("You must choose between 8 and 128");
   } else {
-    var conSpecial = confirm("Would you like to including special characters");
-    var conNumber = confirm("Would you like to including number characters");
-    var conLowercase = confirm(
-      "Would you like to including lowercase characters"
-    );
-    var conUppercase = confirm(
-      "Would you like to including uppercase characters"
-    );
+    // Continues once user input is validated
+    conSpecial = confirm("Would you like to including special characters");
+    conNumber = confirm("Would you like to including number characters");
+    conLowercase = confirm("Would you like to including lowercase characters");
+    conUppercase = confirm("Would you like to including uppercase characters");
   }
 
-  var userChoices;
+  userChoices;
 
+  //if loop for all negative options 
   if (conSpecial) {
     userChoices = special;
   }
@@ -163,10 +83,14 @@ function generator() {
 }
 
 var btn = document.getElementById("btn");
-
 btn.addEventListener("click", function () {
   generator();
-  document.getElementById("password").textContent = userChoices;
+  var userPassword = "";
+  // Random selection for all variables: 
+  for (var i = 0; i < user; i++) {
+    userPassword += userChoices[Math.floor(Math.random() * userChoices.length)];
+  }
+  console.log(userChoices);
+  document.getElementById("password").placeholder = userPassword;
 });
 
-// generator();
